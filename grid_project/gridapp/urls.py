7d7147b547925444cb2c9a82d94760233c522bde
lasyasri09 <23b01a1220@svecw.edu.in>
2025-03-19 14:55:40 +0530

    19-03-25 6

---
+++ grid_project/gridapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path 
+from .views import grid_view 
+ 
+urlpatterns = [ 
+        path("", grid_view, name='grid_view') 
+] 
---
+++ grid_project/gridapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+ 
+def grid_view(request): 
+    return render(request,'grids.html') 
+
+# Create your views here.
