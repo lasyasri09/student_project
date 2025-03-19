5be0b6c345723616eba82cd33f236037c2b9c76e
lasyasri09 <23b01a1220@svecw.edu.in>
2025-03-19 15:00:13 +0530

    19-03-25 a

---
+++ sorting_project/sortingapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+class Person(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    age=models.CharField(max_length=15)
+
+# Create your models here.
